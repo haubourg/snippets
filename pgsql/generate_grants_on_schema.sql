cf504abb676f5f4ee76939c4e1d5bc3be2a32789
Régis Haubourg <regis.haubourg@cstb.fr>
2023-05-25 09:28:11 +0200

    include simpler solution for mass grants

--- pgsql/generate_grants_on_schema.sql
+++ pgsql/generate_grants_on_schema.sql
@@ -1,4 +1,13 @@
-select 'GRANT SELECT ON  '||schemaname||'.'||tablename||' TO gorenove_df_r;'
-from pg_tables where   ( schemaname in ('dgfmpm2022')  )
+# generation des instructions de lecture seule - à appliquer en masse avec \gexec dans psql
+
+GRANT SELECT  ON  ALL TABLES IN SCHEMA [schema_name] TO role_specification ;
+GRANT USAGE ON  ALL SEQUENCES IN SCHEMA [schema_name] TO role_specification ;
+GRANT  EXECUTE ON  ALL ROUTINES  IN SCHEMA [schema_name] TO role_specification ;
+
+
+GRANT SELECT ON  '||schemaname||'.'||tablename||' TO gorenove_df_r;'
+from pg_tables where   ( schemaname in ('[shemaName]')  )
 and tablename not like '%partition%' order by schemaname, tablename;
+
+
 
